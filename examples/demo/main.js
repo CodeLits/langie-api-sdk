5b7b263c3fa7af2e4f7a0c9729425752c70f61df
Codelit <codelits@gmail.com>
2025-06-27 00:35:20 +0200

    WIP

--- examples/demo/main.js
+++ examples/demo/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueSelect from 'vue3-select-component'
 
 const app = createApp(App)
-app.component('VueSelect', VueSelect)
 app.mount('#app')
